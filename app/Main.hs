6a9d31b2ab961698d252fee333ce68a276ff7863
Valmir Pretto <valmirjunior0088@gmail.com>
2021-08-23 20:31:58 -0300

    Handle command line arguments more gracefully

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,21 @@
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
 import System.Exit (die)
 import Ulc (run)
 
+usage :: String -> String
+usage name =
+  "USAGE: " ++ name ++ " MODE FILE OUTPUT"
+
 main :: IO ()
 main = do
-  mode : file : output : [] <- getArgs
+  arguments <- getArgs
+
+  (mode, file, output) <- case arguments of
+    mode : file : output : [] -> return (mode, file, output)
+    _ -> getProgName >>= die . usage
+
   source <- readFile file
+
   case run mode source of
     Left message -> die message
     Right result -> writeFile output result

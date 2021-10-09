a8f13aad5e5f6b95eb08c71f6741a459835b23c0
Valmir Pretto <valmirjunior0088@gmail.com>
2021-10-09 17:24:28 -0300

    Encodings to be used in the WebAssembly backend

---
+++ src/Ulc/WebAssembly/BitDebug.hs
@@ -0,0 +1,31 @@
+module Ulc.WebAssembly.BitDebug
+  (showBits
+  ,showByte
+  ,showBytes
+  )
+  where
+
+import Data.Bits (FiniteBits, finiteBitSize, testBit, shiftR)
+import Data.Word (Word8)
+import Data.Char (toUpper)
+import Data.List (intercalate)
+import Numeric (showHex)
+
+showBits :: FiniteBits a => a -> String
+showBits bits =
+  go (finiteBitSize bits - 1) where
+    go shift =
+      if shift >= 0
+        then
+          let bit = if testBit (shiftR bits shift) 0 then '1' else '0'
+          in bit : go (pred shift)
+        else
+          ""
+
+showByte :: Word8 -> String
+showByte byte =
+  "0x" ++ map toUpper (showHex byte "")
+
+showBytes :: [Word8] -> String
+showBytes =
+  intercalate " " . map showByte
---
+++ src/Ulc/WebAssembly/Ieee754.hs
@@ -0,0 +1,15 @@
+module Ulc.WebAssembly.Ieee754
+  (ieee754single
+  ,ieee754double
+  )
+  where
+
+import Data.Word (Word8)
+
+ieee754single :: Float -> [Word8]
+ieee754single _ =
+  undefined
+
+ieee754double :: Double -> [Word8]
+ieee754double _ =
+  undefined

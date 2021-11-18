e4f9cddfe34b3a7151b98dd58f54eeb6dc0a9eb4
Valmir Pretto <valmir.pretto@perimeter.company>
2021-11-18 18:20:06 -0300

    Use GHC bit pattern for IEEE 754 for now

--- src/Ulc/WebAssembly/Ieee754.hs
+++ src/Ulc/WebAssembly/Ieee754.hs
@@ -5,11 +5,10 @@ module Ulc.WebAssembly.Ieee754
   where
 
 import Data.Word (Word32, Word64)
+import GHC.Float (castFloatToWord32, castDoubleToWord64)
 
 ieee754Single :: Float -> Word32
-ieee754Single _ =
-  undefined
+ieee754Single = castFloatToWord32
 
 ieee754Double :: Double -> Word64
-ieee754Double _ =
-  undefined
+ieee754Double = castDoubleToWord64
